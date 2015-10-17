f3a2b00e6036575b91258509ffa4df228e5e08fa
Pablo de la Peña <p@shedcollective.org>
2015-10-17 13:57:05 +0100

    Updated services to allow for easy app overriding

--- services/services.php
+++ services/services.php
@@ -3,16 +3,32 @@
 return array(
     'factories' => array(
         'TemplateGroup' => function () {
-            return new \Nails\Cms\Template\TemplateGroup();
+            if (class_exists('\App\Cms\Template\TemplateGroup')) {
+                return new \App\Cms\Template\TemplateGroup();
+            } else {
+                return new \Nails\Cms\Template\TemplateGroup();
+            }
         },
         'TemplateArea' => function () {
-            return new \Nails\Cms\Template\TemplateArea();
+            if (class_exists('\App\Cms\Template\TemplateArea')) {
+                return new \App\Cms\Template\TemplateArea();
+            } else {
+                return new \Nails\Cms\Template\TemplateArea();
+            }
         },
         'TemplateOption' => function () {
-            return new \Nails\Cms\Template\TemplateOption();
+            if (class_exists('\App\Cms\Template\TemplateOption')) {
+                return new \App\Cms\Template\TemplateOption();
+            } else {
+                return new \Nails\Cms\Template\TemplateOption();
+            }
         },
         'WidgetGroup' => function () {
-            return new \Nails\Cms\Widget\WidgetGroup();
+            if (class_exists('\App\Cms\Widget\WidgetGroup')) {
+                return new \App\Cms\Widget\WidgetGroup();
+            } else {
+                return new \Nails\Cms\Widget\WidgetGroup();
+            }
         }
     )
 );
