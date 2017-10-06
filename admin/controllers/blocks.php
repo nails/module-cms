643a616af6887d70642829b98664cb39dd4cf856
Pablo de la Peña <p@shedcollective.org>
2017-10-06 14:56:18 +0100

    Tidying up block admin

--- admin/controllers/blocks.php
+++ admin/controllers/blocks.php
@@ -12,9 +12,9 @@
 
 namespace Nails\Admin\Cms;
 
-use Nails\Factory;
 use Nails\Admin\Helper;
 use Nails\Cms\Controller\BaseAdmin;
+use Nails\Factory;
 
 class Blocks extends BaseAdmin
 {
@@ -24,7 +24,7 @@ class Blocks extends BaseAdmin
 
     /**
      * Announces this controller's navGroups
-     * @return stdClass
+     * @return \stdClass
      */
     public static function announce()
     {
@@ -34,6 +34,7 @@ class Blocks extends BaseAdmin
             $oNavGroup->setLabel('CMS');
             $oNavGroup->setIcon('fa-file-text');
             $oNavGroup->addAction('Manage Blocks');
+
             return $oNavGroup;
         }
     }
@@ -46,15 +47,15 @@ class Blocks extends BaseAdmin
      */
     public static function permissions()
     {
-        $permissions = parent::permissions();
+        $aPermissions = parent::permissions();
 
-        $permissions['manage']  = 'Can manage blocks';
-        $permissions['create']  = 'Can create a new block';
-        $permissions['edit']    = 'Can edit an existing block';
-        $permissions['delete']  = 'Can delete an existing block';
-        $permissions['restore'] = 'Can restore a deleted block';
+        $aPermissions['manage']  = 'Can manage blocks';
+        $aPermissions['create']  = 'Can create a new block';
+        $aPermissions['edit']    = 'Can edit an existing block';
+        $aPermissions['delete']  = 'Can delete an existing block';
+        $aPermissions['restore'] = 'Can restore a deleted block';
 
-        return $permissions;
+        return $aPermissions;
     }
 
     // --------------------------------------------------------------------------
@@ -68,20 +69,16 @@ class Blocks extends BaseAdmin
 
         // --------------------------------------------------------------------------
 
-        //  Load Model
-        $this->oBlockModel = Factory::model('Block', 'nailsapp/module-cms');
-
-        // --------------------------------------------------------------------------
-
         //  Define block types; block types allow for proper validation
-        $this->data['blockTypes']              = array();
-        $this->data['blockTypes']['plaintext'] = 'Plain Text';
-        $this->data['blockTypes']['richtext']  = 'Rich Text';
-        $this->data['blockTypes']['image']     = 'Image (*.jpg, *.png, *.gif)';
-        $this->data['blockTypes']['file']      = 'File (*.*)';
-        $this->data['blockTypes']['number']    = 'Number';
-        $this->data['blockTypes']['url']       = 'URL';
-        $this->data['blockTypes']['email']     = 'Email';
+        $this->data['blockTypes'] = [
+            'plaintext' => 'Plain Text',
+            'richtext'  => 'Rich Text',
+            'image'     => 'Image (*.jpg, *.png, *.gif)',
+            'file'      => 'File (*.*)',
+            'number'    => 'Number',
+            'url'       => 'URL',
+            'email'     => 'Email',
+        ];
     }
 
     // --------------------------------------------------------------------------
@@ -93,7 +90,6 @@ class Blocks extends BaseAdmin
     public function index()
     {
         if (!userHasPermission('admin:cms:blocks:manage')) {
-
             unauthorised();
         }
 
@@ -104,65 +100,74 @@ class Blocks extends BaseAdmin
 
         // --------------------------------------------------------------------------
 
-        $tableAlias = $this->oBlockModel->getTableAlias();
+        $oModel      = Factory::model('Block', 'nailsapp/module-cms');
+        $oInput      = Factory::service('Input');
+        $sTableAlias = $oModel->getTableAlias();
 
         // --------------------------------------------------------------------------
 
         //  Get pagination and search/sort variables
-        $page      = $this->input->get('page')      ? $this->input->get('page')      : 0;
-        $perPage   = $this->input->get('perPage')   ? $this->input->get('perPage')   : 50;
-        $sortOn    = $this->input->get('sortOn')    ? $this->input->get('sortOn')    : $tableAlias . '.label';
-        $sortOrder = $this->input->get('sortOrder') ? $this->input->get('sortOrder') : 'desc';
-        $keywords  = $this->input->get('keywords')  ? $this->input->get('keywords')  : '';
+        $iPage      = $oInput->get('page') ? $oInput->get('page') : 0;
+        $iPerPage   = $oInput->get('perPage') ? $oInput->get('perPage') : 50;
+        $sSortOn    = $oInput->get('sortOn') ? $oInput->get('sortOn') : $sTableAlias . '.label';
+        $sSortOrder = $oInput->get('sortOrder') ? $oInput->get('sortOrder') : 'desc';
+        $sKeywords  = $oInput->get('keywords') ? $oInput->get('keywords') : '';
 
         // --------------------------------------------------------------------------
 
         //  Define the sortable columns
-        $sortColumns = array(
-            $tableAlias . '.label'    => 'Label',
-            $tableAlias . '.located'  => 'Location',
-            $tableAlias . '.type'     => 'Type',
-            $tableAlias . '.created'  => 'Created',
-            $tableAlias . '.modified' => 'Modified'
-        );
+        $sortColumns = [
+            $sTableAlias . '.label'    => 'Label',
+            $sTableAlias . '.located'  => 'Location',
+            $sTableAlias . '.type'     => 'Type',
+            $sTableAlias . '.created'  => 'Created',
+            $sTableAlias . '.modified' => 'Modified',
+        ];
 
         // --------------------------------------------------------------------------
 
         //  Checkbox filters
-        $cbFilters   = array();
-        $cbFilters[] = Helper::searchFilterObject(
-            $tableAlias . '.type',
-            'Type',
-            array()
-        );
+        $aCbFilters = [
+            Helper::searchFilterObject(
+                $sTableAlias . '.type',
+                'Type',
+                []
+            ),
+        ];
 
-        foreach ($this->data['blockTypes'] as $slug => $label) {
-
-            $cbFilters[0]->options[] = Helper::searchFilterObjectOption($label, $slug, true);
+        foreach ($this->data['blockTypes'] as $sSlug => $sLabel) {
+            $aCbFilters[0]->options[] = Helper::searchFilterObjectOption($sLabel, $sSlug, true);
         }
 
         // --------------------------------------------------------------------------
 
-        //  Define the $data variable for the queries
-        $data = array(
-            'sort' => array(
-                array($sortOn, $sortOrder)
-            ),
-            'keywords' => $keywords,
-            'cbFilters' => $cbFilters
-        );
+        //  Define the $aData variable for the queries
+        $aData = [
+            'sort'      => [
+                [$sSortOn, $sSortOrder],
+            ],
+            'keywords'  => $sKeywords,
+            'cbFilters' => $aCbFilters,
+        ];
 
         //  Get the items for the page
-        $totalRows            = $this->oBlockModel->countAll($data);
-        $this->data['blocks'] = $this->oBlockModel->getAll($page, $perPage, $data);
+        $iTotalRows           = $oModel->countAll($aData);
+        $this->data['blocks'] = $oModel->getAll($iPage, $iPerPage, $aData);
 
         //  Set Search and Pagination objects for the view
-        $this->data['search']     = Helper::searchObject(true, $sortColumns, $sortOn, $sortOrder, $perPage, $keywords, $cbFilters);
-        $this->data['pagination'] = Helper::paginationObject($page, $perPage, $totalRows);
+        $this->data['pagination'] = Helper::paginationObject($iPage, $iPerPage, $iTotalRows);
+        $this->data['search']     = Helper::searchObject(
+            true,
+            $sortColumns,
+            $sSortOn,
+            $sSortOrder,
+            $iPerPage,
+            $sKeywords,
+            $aCbFilters
+        );
 
         //  Add a header button
         if (userHasPermission('admin:cms:blocks:create')) {
-
             Helper::addHeaderButton('admin/cms/blocks/create', 'Create Block');
         }
 
@@ -180,22 +185,24 @@ class Blocks extends BaseAdmin
     public function edit()
     {
         if (!userHasPermission('admin:cms:blocks:edit')) {
-
             unauthorised();
         }
 
         // --------------------------------------------------------------------------
 
-        $this->data['block'] = $this->oBlockModel->getById($this->uri->segment(5));
+        $oModel = Factory::model('Block', 'nailsapp/module-cms');
+        $oInput = Factory::service('Input');
+        $oUri   = Factory::service('Uri');
+
+        $this->data['block'] = $oModel->getById($oUri->segment(5));
 
         if (!$this->data['block']) {
-
             show_404();
         }
 
         // --------------------------------------------------------------------------
 
-        if ($this->input->post()) {
+        if ($oInput->post()) {
 
             //  Form Validation
             $oFormValidation = Factory::service('FormValidation');
@@ -212,11 +219,11 @@ class Blocks extends BaseAdmin
                 case 'file':
                 case 'image':
                 case 'number':
-                    $oFormValidation->set_rules('value', '', 'numeric|xss_clean');
+                    $oFormValidation->set_rules('value', '', 'numeric');
                     break;
 
                 default:
-                    $oFormValidation->set_rules('value', '', 'xss_clean');
+                    $oFormValidation->set_rules('value', '', '');
                     break;
             }
 
@@ -224,21 +231,17 @@ class Blocks extends BaseAdmin
 
             if ($oFormValidation->run($this)) {
 
-                $aBlockData          = array();
-                $aBlockData['value'] = $this->input->post('value');
+                if ($oModel->update($this->data['block']->id, ['value' => $oInput->post('value')])) {
 
-                if ($this->oBlockModel->update($this->data['block']->id, $aBlockData)) {
-
-                    $this->session->set_flashdata('success', 'Block updated successfully.');
+                    $oSession = Factory::service('Session', 'nailsapp/module-auth');
+                    $oSession->set_flashdata('success', 'Block updated successfully.');
                     redirect('admin/cms/blocks');
 
                 } else {
-
                     $this->data['error'] = 'There was a problem updating the new block.';
                 }
 
             } else {
-
                 $this->data['error'] = lang('fv_there_were_errors');
             }
         }
@@ -270,13 +273,14 @@ class Blocks extends BaseAdmin
     public function create()
     {
         if (!userHasPermission('admin:cms:blocks:create')) {
-
             unauthorised();
         }
 
         // --------------------------------------------------------------------------
 
-        if ($this->input->post()) {
+        $oInput = Factory::service('Input');
+
+        if ($oInput->post()) {
 
             //  Form Validation
             $oFormValidation = Factory::service('FormValidation');
@@ -287,7 +291,7 @@ class Blocks extends BaseAdmin
             $oFormValidation->set_rules('located', '', 'xss_clean');
             $oFormValidation->set_rules('type', '', 'required|callback_callbackBlockType');
 
-            switch ($this->input->post('type')) {
+            switch ($oInput->post('type')) {
                 case 'email':
                     $oFormValidation->set_rules('value', '', 'valid_email|xss_clean');
                     break;
@@ -299,11 +303,11 @@ class Blocks extends BaseAdmin
                 case 'file':
                 case 'image':
                 case 'number':
-                    $oFormValidation->set_rules('value', '', 'numeric|xss_clean');
+                    $oFormValidation->set_rules('value', '', 'numeric');
                     break;
 
                 default:
-                    $oFormValidation->set_rules('value', '', 'xss_clean');
+                    $oFormValidation->set_rules('value', '', '');
                     break;
             }
 
@@ -311,27 +315,28 @@ class Blocks extends BaseAdmin
 
             if ($oFormValidation->run($this)) {
 
-                $aBlockData                = array();
-                $aBlockData['type']        = $this->input->post('type');
-                $aBlockData['slug']        = $this->input->post('slug');
-                $aBlockData['label']       = $this->input->post('label');
-                $aBlockData['description'] = $this->input->post('description');
-                $aBlockData['located']     = $this->input->post('located');
-                $aBlockData['value']       = $this->input->post('value_' . $aBlockData['type']);
+                $aBlockData = [
+                    'type'        => $oInput->post('type'),
+                    'slug'        => $oInput->post('slug'),
+                    'label'       => $oInput->post('label'),
+                    'description' => $oInput->post('description'),
+                    'located'     => $oInput->post('located'),
+                    'value'       => $oInput->post('value_' . $oInput->post('type')),
+                ];
 
-                if ($this->oBlockModel->create($aBlockData)) {
+                $oModel = Factory::model('Block', 'nailsapp/module-cms');
+                if ($oModel->create($aBlockData)) {
 
-                    $this->session->set_flashdata('success', 'Block created successfully.');
+                    $oSession = Factory::service('Session', 'nailsapp/module-auth');
+                    $oSession->set_flashdata('success', 'Block created successfully.');
                     redirect('admin/cms/blocks');
 
                 } else {
-
-                    $this->data['error']  = 'There was a problem creating the new block. ';
-                    $this->data['error'] .= $this->oBlockModel->lastError();
+                    $this->data['error'] = 'There was a problem creating the new block. ';
+                    $this->data['error'] .= $oModel->lastError();
                 }
 
             } else {
-
                 $this->data['error'] = lang('fv_there_were_errors');
             }
         }
@@ -355,35 +360,34 @@ class Blocks extends BaseAdmin
     public function delete()
     {
         if (!userHasPermission('admin:cms:blocks:delete')) {
-
             unauthorised();
         }
 
         // --------------------------------------------------------------------------
 
-        $block = $this->oBlockModel->getById($this->uri->segment(5));
+        $oModel   = Factory::model('Block', 'nailsapp/module-cms');
+        $oUri     = Factory::service('Uri');
+        $oSession = Factory::service('Session', 'nailsapp/module-auth');
 
-        if (!$block) {
+        $oBlock = $oModel->getById($oUri->segment(5));
 
-            $this->session->set_flashdata('error', 'Invalid block ID.');
+        if (!$oBlock) {
+            $oSession->set_flashdata('error', 'Invalid block ID.');
             redirect('admin/cms/blocks');
         }
 
         // --------------------------------------------------------------------------
 
-        if ($this->oBlockModel->delete($block->id)) {
-
-            $status = 'success';
-            $msg    = 'Block was deleted successfully.';
-
+        if ($oModel->delete($oBlock->id)) {
+            $sStatus = 'success';
+            $sMsg    = 'Block was deleted successfully.';
         } else {
-
-            $status = 'error';
-            $msg    = 'Failed to delete block. ';
-            $msg   .= $this->oBlockModel->lastError();
+            $sStatus = 'error';
+            $sMsg    = 'Failed to delete block. ';
+            $sMsg    .= $oModel->lastError();
         }
 
-        $this->session->set_flashdata($status, $msg);
+        $oSession->set_flashdata($sStatus, $sMsg);
         redirect('admin/cms/blocks');
     }
 
@@ -391,7 +395,9 @@ class Blocks extends BaseAdmin
 
     /**
      * Form validation callback: Validates a block's slug
+     *
      * @param  string &$sSlug The slug to validate/sanitise
+     *
      * @return boolean
      */
     public function callbackBlockSlug(&$sSlug)
@@ -400,19 +406,16 @@ class Blocks extends BaseAdmin
         $sSlug = strtolower($sSlug);
 
         $oFormValidation = Factory::service('FormValidation');
+        $oModel          = Factory::model('Block', 'nailsapp/module-cms');
 
         //  Check slug's characters are ok
         if (!preg_match('/[^a-z0-9\-\_]/', $sSlug)) {
 
-            $oBlock = $this->oBlockModel->getBySlug($sSlug);
+            $oBlock = $oModel->getBySlug($sSlug);
 
             if (!$oBlock) {
-
-                //  OK!
                 $bResult = true;
-
             } else {
-
                 $oFormValidation->set_message(
                     'callbackBlockSlug',
                     'Must be unique'
@@ -421,7 +424,6 @@ class Blocks extends BaseAdmin
             }
 
         } else {
-
             $oFormValidation->set_message(
                 'callbackBlockSlug',
                 'Invalid characters: a-z, 0-9, - and _ only, no spaces.'
@@ -435,29 +437,26 @@ class Blocks extends BaseAdmin
 
     /**
      * Form Validation Callback: Validates a block's type
-     * @param  string $type The type to validate
+     *
+     * @param  string $sType The type to validate
+     *
      * @return boolean
      */
-    public function callbackBlockType($type)
+    public function callbackBlockType($sType)
     {
-        $type = trim($type);
-
+        $sType           = trim($sType);
         $oFormValidation = Factory::service('FormValidation');
 
-        if ($type) {
+        if ($sType) {
 
-            if (isset($this->data['blockTypes'][$type])) {
-
+            if (isset($this->data['blockTypes'][$sType])) {
                 return true;
-
             } else {
-
                 $oFormValidation->set_message('callbackBlockType', 'Block type not supported.');
                 return false;
             }
 
         } else {
-
             $oFormValidation->set_message('callbackBlockType', lang('fv_required'));
             return false;
         }
