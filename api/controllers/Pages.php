2d3f2afb1ac29ee21fe5949aa0c4b6e5fad6cbb3
Pablo de la Peña <p@shedcollective.org>
2017-11-30 08:52:06 +0000

    Tidied up wdgete ditor Js and opened permissons of Widgets to all admins

--- api/controllers/Pages.php
+++ api/controllers/Pages.php
@@ -12,9 +12,10 @@
 
 namespace Nails\Api\Cms;
 
+use Nails\Api\Controller\Base;
 use Nails\Factory;
 
-class Pages extends \Nails\Api\Controller\Base
+class Pages extends Base
 {
     /**
      * Require the user be authenticated to use any endpoint
@@ -24,82 +25,37 @@ class Pages extends \Nails\Api\Controller\Base
     // --------------------------------------------------------------------------
 
     /**
-     * Get the widget's editor, preopulated with any POST'ed data
+     * Returns the URL for a page preview
      * @return array
      */
-    public function postWidgetEditor()
-    {
-        $aOut             = array();
-        $sRequestedWidget = $this->input->post('widget');
-
-        if ($sRequestedWidget) {
-
-            parse_str($this->input->post('data'), $aWidgetData);
-
-            $oWidgetModel     = Factory::model('Widget', 'nailsapp/module-cms');
-            $oRequestedWidget = $oWidgetModel->getBySlug($sRequestedWidget);
-
-            if ($oRequestedWidget) {
-
-                $aOut['HTML'] = $oRequestedWidget->getEditor($aWidgetData);
-
-                if (empty($aOut['HTML'])) {
-
-                    $aOut['HTML'] = '<p class="static">This widget has no configurable options.</p>';
-                }
-
-            } else {
-
-                $aOut['status'] = 400;
-                $aOut['error']  = 'Invalid Widget - Error number 2';
-            }
-
-        } else {
-
-            $aOut['status'] = 400;
-            $aOut['error']  = 'Widget slug must be specified - Error number 1';
-        }
-
-        return $aOut;
-    }
-
-    // --------------------------------------------------------------------------
-
     public function postPreview()
     {
+        $oInput     = Factory::service('Input');
         $oPageModel = Factory::model('Page', 'nailsapp/module-cms');
-        $aPageData  = array(
-            'title' => $this->input->post('title'),
-            'slug' => $this->input->post('slug'),
-            'parent_id' => (int) $this->input->post('parent_id'),
-            'template' => $this->input->post('template'),
-            'template_data' => $this->input->post('template_data'),
-            'template_options' => $this->input->post('template_options'),
-            'seo_title' => $this->input->post('seo_title'),
-            'seo_description' => $this->input->post('seo_description'),
-            'seo_keywords' => $this->input->post('seo_keywords')
-        );
-
-        $aPageData['parent_id'] = !empty($aPageData['parent_id']) ? $aPageData['parent_id'] : null;
+        $aPageData  = [
+            'title'            => $oInput->post('title'),
+            'slug'             => $oInput->post('slug'),
+            'parent_id'        => (int) $oInput->post('parent_id') ?: null,
+            'template'         => $oInput->post('template'),
+            'template_data'    => $oInput->post('template_data'),
+            'template_options' => $oInput->post('template_options'),
+            'seo_title'        => $oInput->post('seo_title'),
+            'seo_description'  => $oInput->post('seo_description'),
+            'seo_keywords'     => $oInput->post('seo_keywords'),
+        ];
 
         if (!empty($aPageData['template_options'][$aPageData['template']])) {
-
             $aPageData['template_options'] = $aPageData['template_options'][$aPageData['template']];
             $aPageData['template_options'] = json_encode($aPageData['template_options']);
-
         } else {
-
             $aPageData['template_options'] = null;
         }
 
         $iPreviewId = $oPageModel->createPreview($aPageData);
         if ($iPreviewId) {
-
-            return array('url' => site_url('cms/render/preview/' . $iPreviewId));
-
+            return ['url' => site_url('cms/render/preview/' . $iPreviewId)];
         } else {
-
-            return array('status' => 500, 'error' => $oPageModel->lastError());
+            return ['status' => 500, 'error' => $oPageModel->lastError()];
         }
     }
 }
