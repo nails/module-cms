bd1a27da5ac4343582ad138129e3a39c193ed2a3
Pablo de la Peña <p@shedcollective.org>
2020-01-31 15:33:31 +0000

    Allowing longer CMS block slugs

---
+++ migrations/6.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Migration: 6
+ * Started:   31/01/2020
+ *
+ * @package     Nails
+ * @subpackage  module-cms
+ * @category    Database Migration
+ * @author      Nails Dev Team
+ * @link
+ */
+
+namespace Nails\Database\Migration\Nails\ModuleCms;
+
+use Nails\Common\Console\Migrate\Base;
+
+class Migration6 extends Base
+{
+    /**
+     * Execute the migration
+     * @return Void
+     */
+    public function execute()
+    {
+        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}cms_block` CHANGE `slug` `slug` VARCHAR(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT "";');
+    }
+}
