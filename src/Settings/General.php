55fe6c04cd166135879d9336b49a94b7511412e8
Pablo de la Peña <p@shedcollective.org>
2021-03-25 13:56:02 +0000

    Replace settings with unified settings

---
+++ src/Settings/General.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Nails\Cms\Settings;
+
+use Nails\Cms\Model\Page;
+use Nails\Cms\Service\Driver;
+use Nails\Common\Helper\Form;
+use Nails\Common\Interfaces;
+use Nails\Common\Service\FormValidation;
+use Nails\Components\Setting;
+use Nails\Cms\Constants;
+use Nails\Factory;
+
+/**
+ * Class General
+ *
+ * @package Nails\Cms\Settings
+ */
+class General implements Interfaces\Component\Settings
+{
+    /**
+     * @inheritDoc
+     */
+    public function getLabel(): string
+    {
+        return 'CMS';
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @inheritDoc
+     */
+    public function get(): array
+    {
+        /** @var Page $oPageModel */
+        $oPageModel = Factory::model('Page', Constants::MODULE_SLUG);
+
+        /** @var Setting $oHomepage */
+        $oHomepage = Factory::factory('ComponentSetting');
+        $oHomepage
+            ->setKey('homepage')
+            ->setType(Form::FIELD_DROPDOWN)
+            ->setLabel('Homepage')
+            ->setClass('select2')
+            ->setOptions(['' => 'No Homepage Selected'] + $oPageModel->getAllFlat([
+                    'where' => [
+                        [
+                            'is_published',
+                            true,
+                        ],
+                    ],
+                ]));
+
+        return [
+            $oHomepage,
+        ];
+    }
+}
