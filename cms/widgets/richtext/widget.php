5905c132ba47af49aaa5035d855c7cf58921803f
Pablo de la Peña <p@shedcollective.org>
2021-03-26 15:33:18 +0000

    Remove Bower support

--- cms/widgets/richtext/widget.php
+++ cms/widgets/richtext/widget.php
@@ -28,7 +28,7 @@ class Richtext extends WidgetBase
         $this->description = 'Build beautiful pages using the rich text editor; embed images, links and more.';
         $this->keywords    = 'rich text,formatted text,formatted,wysiwyg,embed';
 
-        $this->assets_editor[] = array('ckeditor/ckeditor.js', 'NAILS-BOWER');
-        $this->assets_editor[] = array('ckeditor/adapters/jquery.js', 'NAILS-BOWER');
+        $this->assets_editor[] = ['https://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.16.0/ckeditor.min.js'];
+        $this->assets_editor[] = ['https://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.16.0/adapters/jquery.min.js'];
     }
 }
