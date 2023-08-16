89af5a3fe2b6d6e3cd5945daa7aa3897da2f691d
Pablo de la Peña <p@shedcollective.org>
2023-08-16 15:31:07 +0100

    feat: Complete replace/delete monitor functionality

--- src/Cdn/Monitor/ObjectIsInTemplateOptions.php
+++ src/Cdn/Monitor/ObjectIsInTemplateOptions.php
@@ -13,6 +13,7 @@ use Nails\Common\Exception\ModelException;
 use Nails\Common\Helper\Model\Condition;
 use Nails\Common\Model\Base;
 use Nails\Common\Resource\Entity;
+use Nails\Common\Service\Database;
 use Nails\Factory;
 
 abstract class ObjectIsInTemplateOptions extends ObjectIsInColumn
@@ -26,6 +27,9 @@ abstract class ObjectIsInTemplateOptions extends ObjectIsInColumn
 
     // --------------------------------------------------------------------------
 
+    /**
+     * @throws FactoryException
+     */
     protected function getModel(): Base
     {
         return Factory::model('Page', Constants::MODULE_SLUG);
@@ -36,6 +40,13 @@ abstract class ObjectIsInTemplateOptions extends ObjectIsInColumn
     protected function getColumn(): string
     {
         return 'template_options';
+    }
+
+    // --------------------------------------------------------------------------
+
+    protected function getDatabaseColumn(): string
+    {
+        return sprintf('%s_%s', $this->getState(), $this->getColumn());
     }
 
     // --------------------------------------------------------------------------
@@ -63,9 +74,8 @@ abstract class ObjectIsInTemplateOptions extends ObjectIsInColumn
         foreach ($aMappings as $sTemplate => $aPaths) {
             foreach ($aPaths as $sPath) {
                 $aConditions[] = sprintf(
-                    'JSON_EXTRACT(`%s_%s`, \'$.%s\') LIKE \'%%"%s"%%\'',
-                    $this->getState(),
-                    $this->getColumn(),
+                    'JSON_EXTRACT(`%s`, \'$.%s\') LIKE \'%%"%s"%%\'',
+                    $this->getDatabaseColumn(),
                     $sPath,
                     $oObject->id,
                 );
@@ -82,12 +92,12 @@ abstract class ObjectIsInTemplateOptions extends ObjectIsInColumn
         $aDetails = [];
         foreach ($aPages as $oPage) {
 
-            $aOptions = $oPage->{$this->getState()}->template_options;
+            $oOptions = $this->getOptionsFromEntity($oPage);
 
             foreach ($aMappings as $sTemplate => $aPaths) {
                 foreach ($aPaths as $sPath) {
                     if ($oPage->{$this->getState()}->template === $sTemplate) {
-                        if ($oObject->id === (int) ($aOptions->{$sPath} ?? null)) {
+                        if ($oObject->id === (int) ($oOptions->{$sPath} ?? null)) {
                             $aDetails[] = $this->createDetail($oPage, ['path' => $sPath]);
                         }
                     }
@@ -100,15 +110,78 @@ abstract class ObjectIsInTemplateOptions extends ObjectIsInColumn
 
     // --------------------------------------------------------------------------
 
+    /**
+     * @throws FactoryException
+     * @throws ModelException
+     */
     public function delete(Detail $oDetail, CdnObject $oObject): void
     {
-        dd(__FILE__, __LINE__);
+        $iId      = $oDetail->getData()->id;
+        $oOptions = $this->getOptionsFromEntityId($iId);
+
+        $oOptions->{$oDetail->getData()->path} = null;
+
+        $this->updateEntity($iId, $oOptions);
     }
 
     // --------------------------------------------------------------------------
 
+    /**
+     * @throws FactoryException
+     * @throws ModelException
+     */
     public function replace(Detail $oDetail, CdnObject $oObject, CdnObject $oReplacement): void
     {
-        dd(__FILE__, __LINE__);
+        $iId      = $oDetail->getData()->id;
+        $oOptions = $this->getOptionsFromEntityId($iId);
+
+        //  Cast as string as that is how it is stored when a value is set via the admin UI
+        $oOptions->{$oDetail->getData()->path} = (string) $oReplacement->id;
+
+        $this->updateEntity($iId, $oOptions);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @throws ModelException
+     */
+    protected function getOptionsFromEntityId(int $iId): \stdClass
+    {
+        $oEntity = $this
+            ->getModel()
+            ->getById($iId);
+
+        return $this->getOptionsFromEntity($oEntity);
+    }
+
+    // --------------------------------------------------------------------------
+
+    protected function getOptionsFromEntity(Entity $oEntity): \stdClass
+    {
+        return $oEntity->{$this->getState()}->{$this->getColumn()};
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @throws FactoryException
+     * @throws ModelException
+     */
+    protected function updateEntity(int $iEntityId, \stdClass $oOptions): void
+    {
+        /**
+         * The Page model is a mess and doesn't support updating individual columns.
+         * Updating this way has knock on effects in terms of data sanity checks, hash
+         * generation and anything which binds to the listeners. Hopefully not a problem
+         * though as we're updating an option rather than anything to do with page generation.
+         */
+
+        /** @var Database $oDb */
+        $oDb = Factory::service('Database');
+        $oDb
+            ->set($this->getDatabaseColumn(), json_encode($oOptions))
+            ->where('id', $iEntityId)
+            ->update($this->getModel()->getTableName());
     }
 }
