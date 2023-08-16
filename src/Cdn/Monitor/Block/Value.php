894806eb201c29fa838007ea3746fcb4167ea1f9
Pablo de la Peña <p@shedcollective.org>
2023-08-16 12:51:41 +0100

    feat: add Block CDN monitor

---
+++ src/Cdn/Monitor/Block/Value.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Nails\Cms\Cdn\Monnitor\Block;
+
+use Nails\Cdn\Cdn\Monitor\ObjectIsInColumn;
+use Nails\Cms\Constants;
+use Nails\Cms\Model\Block;
+use Nails\Common\Helper\Model\WhereIn;
+use Nails\Common\Model\Base;
+
+class Value extends ObjectIsInColumn
+{
+    protected function getModel(): Base
+    {
+        return Factory::model('Block', Constants::MODULE_SLUG);
+    }
+
+    // --------------------------------------------------------------------------
+
+    protected function getColumn(): string
+    {
+        return 'value';
+    }
+
+    // --------------------------------------------------------------------------
+
+    protected function getAdditionalQueryData(): array
+    {
+        /** @var Block $oModel */
+        $oModel = $this->getModel();
+
+        return [
+            new WhereIn('type', [$oModel::TYPE_FILE, $oModel::TYPE_IMAGE]),
+        ];
+    }
+}
